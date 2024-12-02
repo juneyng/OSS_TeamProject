34882ef0b0896650ae760abe8bbc5b79c89626aa
juneyng <juneyng02@gmail.com>
2024-12-02 10:11:57 +0900

    TeammProject-3-API Fetch 구현

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from "react-router-dom";
+// import List from "./page/List";
+// import Create from "./page/Create";
+// import Update from "./page/Update";
+
+// export default function AppRouter() {
+//   return (
+//     // <Router>
+//     //   <Routes>
+//     //     <Route path="/" element={<Navigate to="/list" />} />
+//     //     <Route path="/list" element={<List />} />
+//     //     <Route path="/create" element={<Create />} />
+//     //     <Route path="/update" element={<Update />} />
+//     //   </Routes>
+//     // </Router>
+//   );
+// }
